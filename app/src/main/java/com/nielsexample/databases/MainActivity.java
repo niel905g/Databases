2bbde14ee3e8afbf3d8d2e7823dddd1b35821deb
niel905g <niel905g@edu.easj.dk>
2018-05-23 18:08:15 +0200

    version w/update method

--- app/src/main/java/com/nielsexample/databases/MainActivity.java
+++ app/src/main/java/com/nielsexample/databases/MainActivity.java
@@ -1,48 +1,31 @@
 package com.nielsexample.databases;
 
-import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.main);
         DBAdapter db = new DBAdapter(this);
-/*
-        //---add a contact---
+ /*
+ //---add a contact---
+ ...
+ //--get all contacts---
+ ...
+ //---get a contact---
+ ...
+ db.close();
+ */
+        //---update contact---
         db.open();
-        long id = db.insertContact("Jennifer Ann", "jenniferann@jfdimarzio.com");
-        id = db.insertContact("Oscar Diggs", "oscar@oscardiggs.com");
-        db.close();
-        db.open();
-        Cursor c = db.getAllContacts();
-        if (c.moveToFirst())
-        {
-            do {
-                DisplayContact(c);
-            } while (c.moveToNext());
-        }
-        db.close();
-        */
-//---get a contact---
-        db.open();
-        Cursor c = db.getContact(2);
-        if (c.moveToFirst())
-            DisplayContact(c);
+        if (db.updateContact(1, "Oscar Diggs", "oscar@oscardiggs.com"))
+            Toast.makeText(this, "Update successful.",
+                    Toast.LENGTH_LONG).show();
         else
-            Toast.makeText(this, "No contact found", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Update failed.", Toast.LENGTH_LONG).show();
         db.close();
-    }
-
-    public void DisplayContact(Cursor c)
-    {
-        Toast.makeText(this,
-                "id: " + c.getString(0) + "\n" +
-                        "Name: " + c.getString(1) + "\n" +
-                        "Email: " + c.getString(2),
-                Toast.LENGTH_LONG).show();
     }
 }
